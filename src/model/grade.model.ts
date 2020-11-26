61838b2886d0adb7585a944f0f6458fd46427f8c
divyanair <divunair@gmail.com>
2020-11-26 20:03:33 +0900

    changed teachersection's schema to match requirement

--- src/model/grade.model.ts
+++ src/model/grade.model.ts
@@ -2,9 +2,13 @@ import { model, Schema, Model, Document } from 'mongoose';
 
 export interface IGrade extends Document {
     teacherId: String;
+    firstName:String;
+    lastName:String;
+    dName:String;
     section:  [{
         sectionId: string
         sectionName: string
+        gradeLevel:string
     }
 
 
@@ -15,13 +19,17 @@ export interface IGrade extends Document {
 
 const GradeSchema: Schema = new Schema({
     teacherId: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    dName: { type: String, required: true },
     section: 
              [({
             sectionId: { type: String, required: true },
             sectionName :{  
                 type : String,
                 required: true
-              }
+              },
+              gradeLevel: { type: String, required: true }
             
             }
             
