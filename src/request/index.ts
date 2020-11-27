c204ea214f7bf38b3a9de26a8abc3216e78ab50b
divyanair <divunair@gmail.com>
2020-11-28 08:58:59 +0900

    API for setup dropdown 90% done

--- src/request/index.ts
+++ src/request/index.ts
@@ -2,3 +2,4 @@ export { GradeRequest, Grade } from './grade.request';
 export{StudentRequest,Student} from  './student.request';
 
 export{AssessGradeRequest,AssessGrade} from  './assessgrade.request'
+export{SetUpRequest , SetUp} from  './setup.request'
